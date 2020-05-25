e315a6a612035303d3efc73537cd504cfaf55402
Darius Tan <dariustan97@gmail.com>
2020-05-25 21:03:37 +1200

    Prevent the default copy event.

--- copy.js
+++ copy.js
@@ -1,4 +1,5 @@
 document.addEventListener('copy', (event) => {
 	const selection = document.getSelection();
     chrome.runtime.sendMessage({text: selection.toString()});
+    event.preventDefault()
 })
